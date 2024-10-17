d7b7050a43756e4f78168c29b7728b283de17ed0
Hebert Silva <hebert.silva@arcotech.io>
2024-10-17 13:12:22 -0300

    chore: adds prettier dependency

--- src/core/types/block.ts
+++ src/core/types/block.ts
@@ -3,3 +3,34 @@ import { blockWidth } from "../constants/block";
 export type BlockType = keyof typeof blockWidth;
 
 export type BlockWidth = (typeof blockWidth)[BlockType];
+
+export type BlockItem = {
+  order: 1 | 2 | 3 | 4;
+  initialRow: 1 | 2 | 3 | 4;
+  initialColumn: 1 | 2 | 3 | 4 | 5;
+  finalRow: 1 | 2 | 3 | 4;
+  finalColumn: 1 | 2 | 3 | 4 | 5;
+  widgetType: any;
+};
+
+// Each block contain up to 4 widgets
+export type BlockConfig = {
+  mobile: [
+    BlockItem,
+    BlockItem | undefined,
+    BlockItem | undefined,
+    BlockItem | undefined,
+  ];
+  tablet: [
+    BlockItem,
+    BlockItem | undefined,
+    BlockItem | undefined,
+    BlockItem | undefined,
+  ];
+  desktop: [
+    BlockItem,
+    BlockItem | undefined,
+    BlockItem | undefined,
+    BlockItem | undefined,
+  ];
+};
