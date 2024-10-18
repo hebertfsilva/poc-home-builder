515faf98f5ee6e5c6bf330f82d2b4007d4e09d4c
Hebert Silva <hebert.silva@arcotech.io>
2024-10-18 16:06:24 -0300

    chore: renames block component core types

--- src/core/types/block.ts
+++ src/core/types/block.ts
@@ -1,17 +1,17 @@
 import { blockWidth } from "../constants/block";
-import { WidgetConfig } from "./widget";
+import { WidgetRatio } from "./widget";
 
 export type BlockType = keyof typeof blockWidth;
 
 export type BlockWidth = (typeof blockWidth)[BlockType];
 
 export type BlockItem = {
-  order: 1 | 2 | 3 | 4;
+  order?: 1 | 2 | 3 | 4;
   initialRow: 1 | 2 | 3 | 4;
   initialColumn: 1 | 2 | 3 | 4 | 5;
-  finalRow: 1 | 2 | 3 | 4;
-  finalColumn: 1 | 2 | 3 | 4 | 5;
-  widgetConfig: WidgetConfig;
+  finalRow?: 1 | 2 | 3 | 4;
+  finalColumn?: 1 | 2 | 3 | 4 | 5;
+  widgetType: WidgetRatio;
 };
 
 // Each block contain up to 4 widgets
